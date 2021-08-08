1216736147e8a4944e6954f6340274e79eb1a4ef
Marcos <marquinhooliveira9968@outlook.com>
2021-08-08 03:56:24 -0300

    adicionando um sistema de login e logout

--- escola/notas/views.py
+++ escola/notas/views.py
@@ -3,3 +3,12 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request, "notas/index.html")
+
+def tabelas(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request, "notas/tabelas.html")
